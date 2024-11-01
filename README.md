a4073468be6b73d76911555c835164e948f024db
shyamguptaa <89705591+shyamguptaa@users.noreply.github.com>
2024-11-01 13:38:22 +0530

    Initial

---
+++ README.md
@@ -0,0 +1,11 @@
+## Getting Started
+## Next.js with ANT D Integrated on Next.js@V14.0.4 weith AntDRegistry
+First, run the development server:
+
+```bash
+npm run dev
+
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+"use client"
+import { useEffect } from "react";
+import Image from 'next/image'
+import styles from './page.module.css'
+import { Button, Flex } from 'antd';
+
+
+export default function Home() {
+  
+  return (
+    <>
+      <Button type="primary">Primary Button</Button>
+      Hello
+    </>
+  )
+}
